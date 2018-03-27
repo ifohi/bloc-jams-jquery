191be73c09a1607724ce5c599ba8650b6f6a3c3c
frances <fhoh85@gmail.com>
2018-03-26 22:23:19 -0700

    Previous button functionality

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,12 +1,33 @@
 {
   $('button#play-pause').on('click', function() {
+    // call playPause() to stop or play song
     player.playPause();
+    // Switches between play & pause icon when button is clicked
     $(this).attr('playState', player.playState);
   });
 
-  $('button#next').on('click', function() {
+
+
+  $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+
+    if (previousSongIndex < 0) { return; }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
+
+
+
+
+  $('button#next').on('click', function() {
+    // if playState !== 'playing', return
+    if (player.playState !== 'playing') { return; }
+
+    // get index of the song that is currently playing and add 1
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
 
